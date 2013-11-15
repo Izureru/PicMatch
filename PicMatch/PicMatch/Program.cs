4a447c1a2fdb8359550c523245b5667f59cda249
Izureru <israel.lewis@mnscorp.net>
2013-11-15 10:57:26 +0000

    Updated to test against 4 images

--- PicMatch/PicMatch/Program.cs
+++ PicMatch/PicMatch/Program.cs
@@ -1,42 +1,52 @@
-﻿using System;
+﻿﻿using System;
 using System.Drawing;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using AForge;
 using AForge.Imaging;
 
 
 namespace PicMatch
 {
 
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
 
-            var image1 = new Bitmap(@"D:\GitHub\PicMatch\PicMatch\PicMatch\images\Bitmap2.bmp", false);
-            var image2 = new Bitmap(@"D:\GitHub\PicMatch\PicMatch\PicMatch\images\Bitmap1.bmp", false);
+            var image1 = new Bitmap(@"..\..\images\Bitmap1.bmp", false);
+            var image2 = new Bitmap(@"..\..\images\Bitmap2.bmp", false);
+            var image3 = new Bitmap(@"..\..\images\Bitmap3.bmp", false);
+            var image4 = new Bitmap(@"..\..\images\Bitmap4.bmp", false);
 
-//The class also can be used to get similarity level between two image of the same size, which can be useful to get information about how different/similar are images: 
-// create template matching algorithm's instance
-// use zero similarity to make sure algorithm will provide anything
-ExhaustiveTemplateMatching tm = new ExhaustiveTemplateMatching( 0 );
-// compare two images
-TemplateMatch[] matchings = tm.ProcessImage(image1, image2);
-// check similarity level
+            // create template matching algorithm's instance
+            // use zero similarity to make sure algorithm will provide anything
+            var aa = new ExhaustiveTemplateMatching(0);
+            var ab = new ExhaustiveTemplateMatching(0);
+            var ac = new ExhaustiveTemplateMatching(0);
 
-            
-  Console.WriteLine(matchings[0].Similarity);
-            Console.ReadLine();
+            // compare two images
+            var matchings12 = aa.ProcessImage(image1, image2);
+            var matchings13 = ab.ProcessImage(image1, image3);
+            var matchings14 = ac.ProcessImage(image1, image4);
 
-            //if ( matchings[0].Similarity > 0.95 )
-//{
+            // check similarity level
+            Console.WriteLine("{0}\n{1}\n{2}", matchings12[0].Similarity, matchings13[0].Similarity,
+                matchings14[0].Similarity);
 
-//    // do something with quite similar images
-//}
+            // compare similarity levels and return result
+            if (matchings12[0].Similarity > matchings13[0].Similarity &&
+                matchings12[0].Similarity > matchings14[0].Similarity)
+            {
+                Console.WriteLine("Image 2 is best match");
+            }
+
+            if (matchings13[0].Similarity > matchings12[0].Similarity &&
+                matchings13[0].Similarity > matchings14[0].Similarity)
+            {
+                Console.WriteLine("Image 3 is best match");
+            }
+            else
+            {
+                Console.WriteLine("Image 4 is best match");
+            }
         }
-
-        
     }
 }
