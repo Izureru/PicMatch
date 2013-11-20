998dd0ac4b45f5b260dd93899e4129a0e1be4f02
JulianSandiford <julian.sandiford@mnscorp.net>
2013-11-20 15:57:46 +0000

    WPF version included

--- PicMatch/PicMatch/Program.cs
+++ PicMatch/PicMatch/Program.cs
@@ -14,27 +14,14 @@ namespace PicMatch
     {
         static void Main(string[] args)
         {
-
-            var image1 = new Bitmap(@"D:\GitHub\PicMatch\PicMatch\PicMatch\images\Bitmap2.bmp", false);
-            var image2 = new Bitmap(@"D:\GitHub\PicMatch\PicMatch\PicMatch\images\Bitmap1.bmp", false);
-
-//The class also can be used to get similarity level between two image of the same size, which can be useful to get information about how different/similar are images: 
-// create template matching algorithm's instance
-// use zero similarity to make sure algorithm will provide anything
-ExhaustiveTemplateMatching tm = new ExhaustiveTemplateMatching( 0 );
-// compare two images
-TemplateMatch[] matchings = tm.ProcessImage(image1, image2);
-// check similarity level
-
-            
-  Console.WriteLine(matchings[0].Similarity);
+            var image1 = new Bitmap(@"C:\Users\Julian Sandiford\Documents\GitHub\PicMatch\PicMatch\PicMatch\images\Bitmap2.bmp", false);
+            var image2 = new Bitmap(@"C:\Users\Julian Sandiford\Documents\GitHub\PicMatch\PicMatch\PicMatch\images\Bitmap1.bmp", false);
+            ExhaustiveTemplateMatching tm = new ExhaustiveTemplateMatching( 0 );
+            TemplateMatch[] matchings = tm.ProcessImage(image1, image2);
+            var MatchScore = matchings[0].Similarity;
+            MatchScore = MatchScore * 100;
+            Console.WriteLine("The images match " + MatchScore + "%");
             Console.ReadLine();
-
-            //if ( matchings[0].Similarity > 0.95 )
-//{
-
-//    // do something with quite similar images
-//}
         }
 
         
---
+++ PicMatch_pimped/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using AForge;
+using AForge.Imaging;
+using System.Drawing;
+
+
+namespace PicMatch_pimped
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            dlg.CheckFileExists = true;
+            dlg.Filter = "Bitmap Files (*.bmp)|*.bmp";
+            dlg.Multiselect = false;
+            if (dlg.ShowDialog() != true)
+            {
+                return;
+            }
+            txtPic1.Text = dlg.FileName;
+            ImageSource imageSource = new BitmapImage(new Uri(dlg.FileName));
+            Pic1.Source = imageSource;
+            
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            {
+                Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+                dlg.CheckFileExists = true;
+                dlg.Filter = "Bitmap Files (*.bmp)|*.bmp";
+                dlg.Multiselect = false;
+                if (dlg.ShowDialog() != true)
+                {
+                    return;
+                }
+                txtPic2.Text = dlg.FileName;
+                ImageSource imageSource = new BitmapImage(new Uri(dlg.FileName));
+                Pic2.Source = imageSource;
+            }
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            var image1 = new Bitmap(txtPic1.Text, false);
+            var image2 = new Bitmap(txtPic2.Text, false);
+            ExhaustiveTemplateMatching tm = new ExhaustiveTemplateMatching(0);
+            TemplateMatch[] matchings = tm.ProcessImage(image2, image1);
+            var MatchScore = matchings[0].Similarity;
+            MatchScore = MatchScore * 100;
+            string result = "The images match " + MatchScore + "%";
+            txtScore.Text = result;
+           
+        }
+    }
+}
